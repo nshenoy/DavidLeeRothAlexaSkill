03d2a887e4231330aa32f11a97070f97e8e1385d
nshenoy <nshenoy>
2017-06-18 22:06:41 -0400

    Adds logging to exception filter

--- DavidLeeRothAlexaSkill/Exceptions/JsonExcptionFilter.cs
+++ DavidLeeRothAlexaSkill/Exceptions/JsonExcptionFilter.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 
 namespace DavidLeeRothAlexaSkill.Exceptions
 {
     public class JsonExceptionFilter : IExceptionFilter
     {
+        private readonly ILogger logger;
+
+        public JsonExceptionFilter(ILoggerFactory loggerFactory)
+        {
+            this.logger = loggerFactory.CreateLogger("JsonExceptionFilter");
+        }
         public void OnException(ExceptionContext context)
         {
             var result = new ObjectResult(new
@@ -16,6 +23,8 @@ namespace DavidLeeRothAlexaSkill.Exceptions
 
             result.StatusCode = 500;
             context.Result = result;
+            logger.LogError(context.Exception.Message);
+            logger.LogError(context.Exception.StackTrace);
         }
     }
 }
